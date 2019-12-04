6618c9c933cae44f9d085e38154c621710509399
Isaiah <issyzac.iz@gmail.com>
2019-12-04 10:59:44 +0300

    feat: added Simprints Identification feature

--- sample/src/main/java/org/smartregister/simprint/sample/MainActivity.java
+++ sample/src/main/java/org/smartregister/simprint/sample/MainActivity.java
@@ -9,22 +9,27 @@ import android.widget.TextView;
 
 
 import org.smartregister.simprint.SimPrintsConstantHelper;
+import org.smartregister.simprint.SimPrintsIdentification;
+import org.smartregister.simprint.SimPrintsIdentifyActivity;
 import org.smartregister.simprint.SimPrintsLibrary;
 import org.smartregister.simprint.SimPrintsRegisterActivity;
 import org.smartregister.simprint.SimPrintsRegistration;
 import org.smartregister.simprint.SimPrintsVerifyActivity;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_REGISTER = 1234;
     private static final int REQUEST_CODE_VERIFY = 1334;
+    private static final int REQUEST_CODE_IDENTIFY = 1445;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        SimPrintsLibrary.init(MainActivity.this,"tZqJnw0ajK04LMYdZzyw","test_user");
-        SimPrintsLibrary.init(MainActivity.this,"fUBnpzDdbsCsMp0egCHB","global_module");
+        SimPrintsLibrary.init(MainActivity.this, BuildConfig.SIMPRINTS_PROJECT_ID,"global_module");
 
         findViewById(R.id.capture_finger_print_btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.identify_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SimPrintsIdentifyActivity.startSimprintsIdentifyActivity(MainActivity.this, "global_module", REQUEST_CODE_IDENTIFY);
+            }
+        });
+
     }
 
     @Override
@@ -48,15 +60,25 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK && data !=null){
 
-            SimPrintsRegistration simprintsRegistration =(SimPrintsRegistration) data.getSerializableExtra(SimPrintsConstantHelper.INTENT_DATA);
-
             switch (requestCode){
                 case  REQUEST_CODE_REGISTER:
+                    SimPrintsRegistration simprintsRegistration =(SimPrintsRegistration) data.getSerializableExtra(SimPrintsConstantHelper.INTENT_DATA);
                     ((TextView)findViewById(R.id.text_status)).setText("GUID:"+simprintsRegistration.getGuid()+":status:"+simprintsRegistration.getCheckStatus());
                     break;
                 case REQUEST_CODE_VERIFY:
+                    simprintsRegistration = (SimPrintsRegistration) data.getSerializableExtra(SimPrintsConstantHelper.INTENT_DATA);
                     ((TextView)findViewById(R.id.text_status)).setText("verification status:"+simprintsRegistration.getCheckStatus());
                     break;
+                case REQUEST_CODE_IDENTIFY:
+                    ArrayList<SimPrintsIdentification> identifications = (ArrayList<SimPrintsIdentification>) data.getSerializableExtra(SimPrintsConstantHelper.INTENT_DATA);
+                    StringBuilder identificationsResults = new StringBuilder();
+                    for (SimPrintsIdentification identification : identifications){
+                        identificationsResults.append(identification.getGuid());
+                        identificationsResults.append("\n");
+                    }
+                    ((TextView)findViewById(R.id.text_status)).setText("Identification Results:"+identificationsResults);
+                    break;
+
             }
         }
     }
