73a8a8498ba4d84b395037f7dc9252ee52b8a30e
Martin Ndegwa <ndegwamartin@gmail.com>
2021-05-12 08:52:31 +0300

    Migrate to Android X

--- sample/src/main/java/org/smartregister/simprint/sample/MainActivity.java
+++ sample/src/main/java/org/smartregister/simprint/sample/MainActivity.java
@@ -1,8 +1,8 @@
 package org.smartregister.simprint.sample;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
